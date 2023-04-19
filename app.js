eca49d109b531cd65c7b6cc7ce660769f90baf5f
Dox <doxthefox@gmail.com>
2023-04-19 08:03:06 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const client_id = '04a301ccb9094be690ff7fea8d0d4db2';
+const redirect_uri = 'http://localhost:8888/callback';
+
+const app = express();
+
+app.get('/login', function (req, res) {
+
+    const state = generateRandomString(16);
+    const scope = 'user-read-private user-read-email';
+
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+});
+
+app.get('/callback', function (req, res) {
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+
+    if (state === null) {
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    } else {
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer.fromn(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+        };
+    }
+});
---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("here");
+const clientId = "04a301ccb9094be690ff7fea8d0d4db2";
+const scope = "user-read-email";
+const redirectUri = "http://localhost:5500"
+const state = crypto.randomUUID().split('-').join('');
+
+if (!location.search) {
+    location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&state=${state}`
+}
+
+const params = location.search.substring(1).split("&").map(x => {
+    const q = x.split('=');
+    return { [q[0]]: q[1] };
+});
+
+console.log(params);
