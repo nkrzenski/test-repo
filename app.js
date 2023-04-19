1b5bf3640a420662e9cb3dbaa3831034b6c17dbd
nkrzenski <doxthefox@gmail.com>
2023-04-19 11:55:42 -0400

    update

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+const express = require('express');
+const crypto = require('crypto');
+const qs = require('qs')
 const client_id = '04a301ccb9094be690ff7fea8d0d4db2';
-const redirect_uri = 'http://localhost:8888/callback';
+const redirect_uri = 'http://localhost:5500';
+const port = 8888;
 
 const app = express();
 
 app.get('/login', function (req, res) {
 
-    const state = generateRandomString(16);
+    const state = crypto.randomUUID().split('-').join('');
     const scope = 'user-read-private user-read-email';
 
     res.redirect('https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
+        qs.stringify({
             response_type: 'code',
             client_id: client_id,
             scope: scope,
@@ -43,3 +47,7 @@ app.get('/callback', function (req, res) {
         };
     }
 });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
