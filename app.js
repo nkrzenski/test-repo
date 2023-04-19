3e0d45ff1d0528ffd4ac55c527ee2a75f0baa129
nkrzenski <doxthefox@gmail.com>
2023-04-19 17:10:55 -0400

    update

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const crypto = require('crypto');
-const qs = require('qs')
+const qs = require('qs');
+const axios = require('axios');
 const client_id = '04a301ccb9094be690ff7fea8d0d4db2';
-const redirect_uri = 'http://localhost:5500';
+const client_secret = process.env.CLIENT_SECRET;
+let redirect_uri = process.env.REDIRECT_URI || 'http://localhost:8888/callback';
+const stateKey = 'spotify_auth_state';
 const port = 8888;
 
 const app = express();
 
 app.get('/login', function (req, res) {
 
-    const state = crypto.randomUUID().split('-').join('');
     const scope = 'user-read-private user-read-email';
+    const state = crypto.randomUUID().split('-').join('');
+
+    res.cookie(stateKey, state);
 
     res.redirect('https://accounts.spotify.com/authorize?' +
         qs.stringify({
@@ -22,32 +27,39 @@ app.get('/login', function (req, res) {
         }));
 });
 
-app.get('/callback', function (req, res) {
+app.get('/callback', async function (req, res) {
 
-    var code = req.query.code || null;
-    var state = req.query.state || null;
+    const code = req.query.code || null;
+    const state = req.query.state || null;
 
     if (state === null) {
         res.redirect('/#' +
-            querystring.stringify({
+            qs.stringify({
                 error: 'state_mismatch'
             }));
     } else {
         var authOptions = {
+            method: "post",
             url: 'https://accounts.spotify.com/api/token',
-            form: {
+            data: {
                 code: code,
                 redirect_uri: redirect_uri,
                 grant_type: 'authorization_code'
             },
             headers: {
-                'Authorization': 'Basic ' + (new Buffer.fromn(client_id + ':' + client_secret).toString('base64'))
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
             },
             json: true
         };
+        const r = await axios(authOptions);
+
+        res.cookie("access_token", r.data.access_token, { secure: process.env.NODE_ENV !== "development", httpOnly: true });
+        res.cookie("refresh_token", r.data.refresh_token, { secure: process.env.NODE_ENV !== "development", httpOnly: true });
+        res.redirect(`${req.headers.referer}`);
     }
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
--- main.js
+++ main.js
@@ -4,13 +4,9 @@ const scope = "user-read-email";
 const redirectUri = "http://localhost:5500"
 const state = crypto.randomUUID().split('-').join('');
 
-// if (!location.search) {
-//     location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&state=${state}`
-// }
+const hash = location.hash.substring(1).split("&").map(x => {
+    const q = x.split('=');
+    return { [q[0]]: q[1] };
+});
 
-// const params = location.search.substring(1).split("&").map(x => {
-//     const q = x.split('=');
-//     return { [q[0]]: q[1] };
-// });
-
-// console.log(params);
+console.log(hash);
