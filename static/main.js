7fdaeb7ea67a8423e8698a5d9f746e20a247f6b7
nkrzenski <doxthefox@gmail.com>
2023-04-25 08:05:05 -0400

    styles for mobile

--- static/main.js
+++ static/main.js
@@ -105,10 +105,10 @@ function renderFound(found) {
                     <svg role="img" height="16" width="16" viewBox="0 0 16 16" class="Svg-ytk21e-0 jAKAlG"><path d="M10 2v9.5a2.75 2.75 0 11-2.75-2.75H8.5V2H10zm-1.5 8.25H7.25A1.25 1.25 0 108.5 11.5v-1.25z"></path></svg>
                 </div>
                 <div class="track-content">
-                    <a class="track-name" href="${track.uri}" target="_blank">${track.name}</a>
+                    <span class="track-name">${track.name}</span>
                     <span class="artists">${artists.join(', ')} · ${track.album.name}</span>
                 </div>
-                <div class="open-track-btn"><button data-href="${track.external_urls.spotify}" data-uri="${track.uri}"><img src="Spotify_Icon_RGB_White.png" />Play on Spotify</button></div>
+                <div class="open-track-btn"><button class="open-spotify" data-href="${track.external_urls.spotify}" data-uri="${track.uri}"><img src="Spotify_Icon_RGB_White.png" />Play on Spotify</button></div>
                 </div>
             `);
         }
